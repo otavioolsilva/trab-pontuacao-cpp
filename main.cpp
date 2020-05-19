695a36b9ac37480a619b977396095b31397ce0f7
Otavio Silva <otavioolsilva@gmail.com>
2020-05-19 07:53:06 -0300

    Leitura dos outros dados

--- main.cpp
+++ main.cpp
@@ -9,14 +9,22 @@ int main()
     cout << "--------------------------------" << endl;
     cout << "INFORME OS DADOS DO ULTIMO ANO" << endl << endl;
 
-    int qCompras;
+    //Variaveis
+    int qCompras, qInadimplencia;
     double ticket;
+    string formaPagamento;
 
     //Leitura de volume
     cout << "Quantas compras o cliente fez no ultimo ano? ";
     cin >> qCompras;
     cout << "Qual o ticket medio? ";
     cin >> ticket;
+
+    //Leitura demais dados
+    cout << endl << "Quantas vezes o cliente atrasou o pagamento? ";
+    cin >> qInadimplencia;
+    cout << "A maioria das compras foi em dinheiro, cartao, ou boleto (D/C/B)? ";
+    cin >> formaPagamento;
 
     return 0;
 }

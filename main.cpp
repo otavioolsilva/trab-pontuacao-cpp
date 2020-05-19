34a331be8b7e97f15ee8355cfb8479ffaf36442d
Otavio Silva <otavioolsilva@gmail.com>
2020-05-19 07:45:30 -0300

    Cabecalho

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    //Cabecalho
+    cout << "SISTEMA DE PERFIL DE CLIENTE" << endl;
+    cout << "--------------------------------" << endl;
+    cout << "INFORME OS DADOS DO ULTIMO ANO" << endl << endl;
+
     return 0;
 }

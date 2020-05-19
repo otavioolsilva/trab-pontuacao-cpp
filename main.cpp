c37bea0a7a7875191e8ad07db3941f0d4c785329
Otavio Silva <otavioolsilva@gmail.com>
2020-05-19 07:48:21 -0300

    Leitura do volume

--- main.cpp
+++ main.cpp
@@ -9,5 +9,14 @@ int main()
     cout << "--------------------------------" << endl;
     cout << "INFORME OS DADOS DO ULTIMO ANO" << endl << endl;
 
+    int qCompras;
+    double ticket;
+
+    //Leitura de volume
+    cout << "Quantas compras o cliente fez no ultimo ano? ";
+    cin >> qCompras;
+    cout << "Qual o ticket medio? ";
+    cin >> ticket;
+
     return 0;
 }
